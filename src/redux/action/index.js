032398de54efd40f0f69ec568935e9e06d934e53
AlvaroCordoba-5 <cordobaalvaro16@gmail.com>
2022-06-28 17:32:52 -0300

    Update index.js

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const GET_MOVIES="GET_MOVIES"
+export const SEARCH_MOVIE="SEARCH_MOVIE"
 
 
 export const getMovies=()=>{
@@ -10,5 +11,15 @@ export const getMovies=()=>{
         .then(res=>res.data)
         .catch(err=>console.error(err))
         dispatch({type:GET_MOVIES,payload:response})
+    }
+}
+
+export const searchMovie=(name)=>{
+    return async function(dispatch){
+        const res = await axios
+        .get(`https://api.themoviedb.org/3/search/movie?api_key=5ad1a1125f0c1d3d69c2569e46f9b71b&query=${name}`)
+        .then(response=>response.data)
+        .catch(err=>console.error(err))
+        dispatch({type:SEARCH_MOVIE,payload:res})
     }
 }
